1a14af93f3e465715de8c51bccd4b6eb35579d36
John Clark <jpc198@icloud.com>
2025-11-28 21:41:41 -0500

    testing

--- app.py
+++ app.py
@@ -11,8 +11,37 @@ from models.arrow import Arrow
 
 
 
+
+
 def main():
-    pass
+    # Setup display
+    pygame.init()
+
+    WIDTH, HEIGHT = 800, 600
+    TOOLBAR_HEIGHT = 25
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    grid_surface = pygame.Surface((WIDTH, HEIGHT))
+    toolbar_surface = pygame.Surface((WIDTH, TOOLBAR_HEIGHT))
+    pygame.display.set_caption("Structural Analysis Tool")
+    clock = pygame.time.Clock()
+    grid_size = 25
+
+    grid = Grid(WIDTH, HEIGHT, grid_size)
+
+    toolbar = Toolbar(TOOLBAR_HEIGHT, WIDTH)
+    toolbar.add_button("Place Node")
+    toolbar.add_button("Connect Node")
+    toolbar.add_button("Reaction Force")
+    toolbar.add_button("Delete Node")
+    toolbar.add_button("Generate force")
+    toolbar.add_button("Visualize forces")
+
+    
+
+
+def draw(screen):
+    screen.fill((0,0,0))
+
 
 if __name__ == "__main__":
     main()
